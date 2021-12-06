71d26a382eecef3ee7adb099b30de96aac9d2ee4
ryanlavu <ryanvu@ucsb.edu>
2021-12-05 23:48:36 -0800

    connected components done

--- GraphOperator.cpp
+++ GraphOperator.cpp
@@ -11,8 +11,9 @@ class GraphOperator {
 
 		map<int, bool> visitedDFS;
 		map<int, bool> visitedBFS;
-		bool visitedAllBFS;
-		vector<int>  vectorBFS; 
+		
+		vector<vector<int>>  vectorBFS; 
+		int bfsVectorCounter = 0;
 
 		// function that uses DFS to check for cycles
 		bool isAcyclic(int start, GraphGenerator graph) {
@@ -33,28 +34,51 @@ class GraphOperator {
 		// function that uses BFS to print all connected components
 		void connectedComponents(int start, GraphGenerator graph, Node * root) {
 			
-			if(!visitedBFS.find(start))
-			{
-				visitedBFS[start] = true;
-				llNode * curr = graph.getAdjList(start);
-
-				while(curr->next)
-				{	
-					if(!visitedBFS.find(curr->next->number))
-					{
-						vectorBFS.push_back(curr->next->number);
-						visitedBFS[curr->next->number] = true;
-					}
-				}
-
-			}
-
+			//Find the connected component of the root argument vertice
+			findConnectedComponents(start, graph, root);
+			
+			//Go through entire AVL tree to find all connected components
 			Node * leftNode = root->leftChild;
 			Node * rightNode = root->rightChild;
 			if(leftNode != null)
 				connectedComponents(leftNode->number, graph, leftNode);
 			if(rightNode != null)
 				connectedComponents(rightNode->number, graph, rightNode);
+
+		}
+
+		void findConnectedComponents(int start, GraphGenerator graph, Node * root){
+			//create a list of connected vertices
+			vector<int> queueAdj;
+			
+			//if the starting vertice is already visited, no use in continuing as its connected component already found
+			if(!visitedBFS.find(start))
+                        {
+				//Found a new component, increment the 2d vector
+				bfsVectorCounter++;
+
+				//If starting vertice not visited, mark it as visited and push 
+                                visitedBFS[start] = true;
+				vectorBFS[bfsVectorCounter].push_back(start);	
+
+                                llNode * curr = graph.getAdjList(start);
+				queueAdj.push_back(start);
+
+				while(!queueAdj.empty)
+				{
+					curr = graph.getAdjList(queueAdj.pop_back)();
+                                	while(curr->next)
+                                	{
+                                        	if(!visitedBFS.find(curr->next->number))
+                                        	{
+                                                	queueAdj.push_back(curr->next->number);
+                                                	visitedBFS[curr->next->number] = true;
+							vectorBFS[bfsVectorCounter].push_back(curr->next->number);
+                                        	}
+                                	}
+				}
+                        }
+
 
 		}
 

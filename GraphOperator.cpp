b781e9818a0250fc40602e1decbad8bedd142914
Matthew Tran <matthewgtran@umail.ucsb.edu>
2021-12-05 17:12:43 -0800

    started graphoperator

---
+++ GraphOperator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "GraphGenerator.cpp"
+
+using namespace std;
+
+class GraphOperator {
+
+	public:
+
+		map<int, bool> visitedDFS;
+		map<int, bool> visitidBFS;
+
+		// function that uses DFS to check for cycles
+		bool isAcyclic(int start) {
+
+			
+
+		}
+
+		// function that uses BFS to print all connected components
+		void connectedComponents(int start) {
+
+
+
+		}
+
+}
